a26c4bb3b6521c75b917688f147bcf6a9d5aac6f
lastbyte <last00000000@gmail.com>
2018-07-11 22:46:41 +0300

    Обавлена информация в обработке ошибок.
    Добавлены трейсы и $previous при обработке ошибок.

--- src/Core/Error/ErrorHandler.php
+++ src/Core/Error/ErrorHandler.php
@@ -6,6 +6,7 @@ namespace SLTest\Core\Error;
 use SLTest\Core\HTTP\Exception\HttpException;
 use SLTest\Core\HTTP\Helper;
 use SLTest\Core\Http\Response;
+use SLTest\Core\Kernel\Kernel;
 use SLTest\Core\View\Format\HtmlFormat;
 use SLTest\Core\View\View;
 
@@ -18,7 +19,17 @@ class ErrorHandler
      */
     public function exceptionHandler(\Throwable $e)
     {
-        $view = new View(new HtmlFormat(), $e->getMessage(), ['template' => 'error.html']);
+        $message = $e->getMessage();
+        if (!empty(Kernel::config()['debug'])) {
+            $message .= "\n" . $e->getTraceAsString();
+            $prev = $e->getPrevious();
+            while ($prev) {
+                $message .= "\n" . $prev->getMessage() . "\n" . $prev->getTraceAsString();
+                $prev = $prev->getPrevious();
+            }
+        }
+
+        $view = new View(new HtmlFormat(), $message, ['template' => 'error.html']);
         $response = $view->getResponse();
         if ($e instanceof HttpException) {
             $response->setStatus($e->getHttpStatusCode());
