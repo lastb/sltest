8f6a3debd9ca79bc9737b22ca6f2b7327508a710
lastbyte <last00000000@gmail.com>
2018-07-13 09:41:07 +0300

    Fix: дефолтный конфиг db.

--- config/config.php
+++ config/config.php
@@ -4,6 +4,7 @@ return array(
     '@data' => array(
         'database' => array(
             'driver' => 'mysql',
+            'user' => 'sltest',
             'db' => 'sltest',
             'host' => 'localhost',
             'port' => 3306,
@@ -11,9 +12,11 @@ return array(
             'prefix' => 'slt'
         ),
         'pdo' => array(
-            'ATTR_ERRMODE' => 'ERRMODE_EXCEPTION',
-            'ATTR_DEFAULT_FETCH_MODE' => 'FETCH_ASSOC',
-            'ATTR_ORACLE_NULLS' => 'NULL_TO_STRING'
+            'attributes' => array(
+                'ATTR_ERRMODE' => 'ERRMODE_EXCEPTION',
+                'ATTR_DEFAULT_FETCH_MODE' => 'FETCH_ASSOC',
+                'ATTR_ORACLE_NULLS' => 'NULL_TO_STRING'
+            )
         ),
         'debug' => true,
         'theme' => 'rocket'
